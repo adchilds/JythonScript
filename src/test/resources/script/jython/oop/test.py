07c538ef113591957c0f1272b5b14e2b15f03578
Adam Childs <adchilds@eckerd.edu>
2015-11-07 12:55:00 -0600

    Update documentation

--- src/test/resources/script/jython/oop/test.py
+++ src/test/resources/script/jython/oop/test.py
@@ -2,14 +2,13 @@ from java.lang import System
 
 class SomeClass:
     """
-
+    Simple class for unit testing OOP (Object Oriented Programming) with Jython.
     """
 
 
     def __init__(self):
         """
-
-        :return:
+        Initializes this object's state.
         """
         self.delta = 7
 
@@ -19,11 +18,7 @@ class SomeClass:
 
     def compute(self, x, y):
         """
-
-        :rtype: object
-        :param x:
-        :param y:
-        :return:
+        Returns the product of a simple computation on the given x and y.
         """
         x = x + self.delta
         y = y - self.delta
